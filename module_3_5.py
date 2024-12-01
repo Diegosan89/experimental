c48b3e5a7dc828df5fbaf39b174bce206221657b
Diegosan89 <pe.scadosony@gmail.com>
2024-12-01 12:13:32 +0500

    second file

---
+++ module_3_5.py
@@ -0,0 +1,23 @@
+# Рекурсия
+from gc import is_finalized
+
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+
+    if len(str_number) < 2:
+        if int(str_number[0]) == 0:
+            return 1
+        else:
+            return first
+
+    else:
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+
+result = get_multiplied_digits(40203)
+print(result)
+
+result2 = get_multiplied_digits(402030)
+print(result2)
