9d0c1f1c01770dc371e14774e9da9a4a75ae437b
Diegosan89 <pe.scadosony@gmail.com>
2024-12-01 22:04:42 +0500

    new_module

---
+++ module_1_3a.py
@@ -0,0 +1,7 @@
+# Переменные
+
+homeworks_done = 12
+hours_to_homeworks = 1.5
+course_name = 'Python'
+time_of_one_homework = hours_to_homeworks / homeworks_done
+print('Курс: '+str(course_name) + ', всего задач: ' + str(homeworks_done) + ', затрачено время: '+ str(hours_to_homeworks) + ', среднее время выполнения: ', time_of_one_homework)
