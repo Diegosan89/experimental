60c2fdbfabd75fd09b51e93a24c70dd02a696fb4
Diegosan89 <pe.scadosony@gmail.com>
2024-12-03 13:42:30 +0500

    рпрп

---
+++ Module_4/module_2_5.py
@@ -0,0 +1,24 @@
+# Функции в Python.Функция с параметром
+
+
+def get_matrix (n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+
+print(result1)
+print(result2)
+print(result3)
+
+# result = get_matrix(int(input ('Введите число строк: ')),
+#                     int(input ('Введите число столбцов: ')),
+#                     int(input ('Введите значение ячейки: ')))
+#
+# print(result)
